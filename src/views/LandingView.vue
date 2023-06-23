5a853e92339d10977fb04f50406d57593096c373
Raquel Martínez <raquel.mrti@gmail.com>
2023-06-23 19:31:57 +0200

    make landing page

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -3,15 +3,20 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-    <img src="http://via.placeholder.com/640x360" alt="" />
-    <h1 class="title">TinyJournal</h1>
-    <h2 class="subtitle">A simple and easy-to-use online journal</h2>
-    <p>
-      Build your own little private space to jot down your thoughts and daily occurrences. With its
-      simple interface, TinyJournal allows you to start writing in no time!
-    </p>
-
-<button type="button" class="button is-primary">Get started</button>
+  <div class="container has-text-centered px-3">
+    <div class="columns">
+      <div class="column is-10 mx-auto">
+        <img src="http://via.placeholder.com/640x360" alt="" />
+        <h1 class="title">TinyJournal</h1>
+        <h2 class="subtitle">A simple and easy-to-use online journal</h2>
+        <p>
+          Build your own little private space to jot down your thoughts and daily occurrences. With
+          its simple interface, TinyJournal allows you to start writing in no time!
+        </p>
+        <button type="button" class="button is-primary mt-4">Get started</button>
+      </div>
+    </div>
+  </div>
 
   <!-- <router-link :to="{ name: 'sign-in' }">Get started</router-link> -->
 </template>
